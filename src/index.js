34e3caa2f67727a91c7ee1ddc6fe7ea8d99438aa
asiyanbi mubashir <asiyanbimubashir@gmail.com>
2021-07-06 00:05:05 +0100

    feat: validaton error handler

--- src/index.js
+++ src/index.js
@@ -1,87 +1,9 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const { v1: uid }= require("uuid")
 const mongoose = require("mongoose")
 const {DBUrl} = require("./utils")
 const Book = require("./models/book")
 const Author = require('./models/author')
-
-// let authors = [
-//   {
-//     name: 'Robert Martin',
-//     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-//     born: 1952,
-//   },
-//   {
-//     name: 'Martin Fowler',
-//     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-//     born: 1963
-//   },
-//   {
-//     name: 'Fyodor Dostoevsky',
-//     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-//     born: 1821
-//   },
-//   { 
-//     name: 'Joshua Kerievsky', // birthyear not known
-//     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-//   },
-//   { 
-//     name: 'Sandi Metz', // birthyear not known
-//     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-//   },
-// ]
-
-// let books = [
-//   {
-//     title: 'Clean Code',
-//     published: 2008,
-//     author: 'Robert Martin',
-//     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring']
-//   },
-//   {
-//     title: 'Agile software development',
-//     published: 2002,
-//     author: 'Robert Martin',
-//     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-//     genres: ['agile', 'patterns', 'design']
-//   },
-//   {
-//     title: 'Refactoring, edition 2',
-//     published: 2018,
-//     author: 'Martin Fowler',
-//     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring']
-//   },
-//   {
-//     title: 'Refactoring to patterns',
-//     published: 2008,
-//     author: 'Joshua Kerievsky',
-//     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring', 'patterns']
-//   },  
-//   {
-//     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-//     published: 2012,
-//     author: 'Sandi Metz',
-//     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-//     genres: ['refactoring', 'design']
-//   },
-//   {
-//     title: 'Crime and punishment',
-//     published: 1866,
-//     author: 'Fyodor Dostoevsky',
-//     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-//     genres: ['classic', 'crime']
-//   },
-//   {
-//     title: 'The Demon ',
-//     published: 1872,
-//     author: 'Fyodor Dostoevsky',
-//     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-//     genres: ['classic', 'revolution']
-//   },
-// ]
 
 mongoose.connect(DBUrl,{
   useUnifiedTopology:true,
@@ -169,20 +91,29 @@ const resolvers = {
         const book = new Book({...args})
         return book.save()
     },
-    editAuthor: (root, args) => {
+    editAuthor: async(root, args) => {
 
         if(!args.setBornTo) return null
-        const authorExist = Author.find({name:args.name})
-        if(!authorExist) return null
+        const authorExist = await Author.find({name:args.name})
+        // if(!authorExist) return null
         authorExist.born = args.setBornTo
-        return authorExist.save()
-  },
+        try {
+         await authorExist.save()
+          
+        } catch (error) {
+           throw new UserInputError(error.message,{
+             invalidArgs:args,
+           })
+        }
 
+        return authorExist
+  },
   Author: {
       bookCount: async(root) => {
         let authorBooks = await Book.find({author:root.name})
         return authorBooks.length
       }
+  }
   }
 }
 
